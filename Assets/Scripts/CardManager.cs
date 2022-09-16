a8fcb07203fe3cc3c3e2b8455f156218bef3e6e9
als8921 <als8921@naver.com>
2022-09-17 00:14:47 +0900

    클릭한 카드 정보 띄우기

---
+++ Assets/Scripts/CardManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class CardManager : MonoBehaviour
+{
+    public GameObject myDeck;
+    GameObject[] myCard = new GameObject[9];
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            myCard[i] = myDeck.transform.GetChild(i).gameObject;
+            myCard[i].transform.GetChild(0).GetComponent<Text>().text = i.ToString();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SelectCardBtn(){
+        Debug.Log(EventSystem.current.currentSelectedGameObject.transform.GetChild(0).GetComponent<Text>().text);
+    }
+}
--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class test : MonoBehaviour
 {
+    public GameObject myDeck;
+    GameObject[] myCard = new GameObject[9];
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < 9; i++)
+        {
+            myCard[i] = myDeck.transform.GetChild(i).gameObject;
+            myCard[i].transform.GetChild(0).GetComponent<Text>().text = i.ToString();
+        }
     }
 
     // Update is called once per frame
