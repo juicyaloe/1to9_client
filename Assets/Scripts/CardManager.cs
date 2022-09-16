355029e4d4bf0d0e73e7ad06cbad756519df7645
als8921 <als8921@naver.com>
2022-09-17 01:00:09 +0900

    대충 틀을 완성했달까?

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -7,25 +7,63 @@ using UnityEngine.EventSystems;
 
 public class CardManager : MonoBehaviour
 {
-    public GameObject myDeck;
+    public GameObject myDeckGameObject;
+    public GameObject mySelectedCardGameObject;
+    public GameObject enemySelectedCardGameObject;
+
+    public Text scoreText;
+
+    int myScore = 0;
+    int enemyScore = 0;
+
+    int myCurrentNum = 0;
+    int enemyCurrentNum = 0;
+
     GameObject[] myCard = new GameObject[9];
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < 9; i++)
         {
-            myCard[i] = myDeck.transform.GetChild(i).gameObject;
-            myCard[i].transform.GetChild(0).GetComponent<Text>().text = i.ToString();
+            myCard[i] = myDeckGameObject.transform.GetChild(i).gameObject;
+            myCard[i].transform.GetChild(0).GetComponent<Text>().text = (i+1).ToString();
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        OnStage();
     }
 
+
+
     public void SelectCardBtn(){
-        Debug.Log(EventSystem.current.currentSelectedGameObject.transform.GetChild(0).GetComponent<Text>().text);
+        string numStr = EventSystem.current.currentSelectedGameObject.transform.GetChild(0).GetComponent<Text>().text;
+        myCurrentNum = int.Parse(numStr);
+        mySelectedCardGameObject.transform.GetChild(0).GetComponent<Text>().text = numStr;
+        Destroy(EventSystem.current.currentSelectedGameObject);
+
+        getEnemyInfo();
+    }
+    
+    void OnStage(){
+        if(myCurrentNum != 0 && enemyCurrentNum != 0)
+        {   
+            Debug.Log(myCurrentNum + "><" + enemyCurrentNum);
+            if(myCurrentNum > enemyCurrentNum)
+                myScore++;
+            else if(myCurrentNum < enemyCurrentNum)
+                enemyScore++;
+            scoreText.text = myScore.ToString() + " vs " + enemyScore.ToString();
+            myCurrentNum = 0;
+            enemyCurrentNum = 0;
+        }
+    }
+
+    void getEnemyInfo()
+    {
+        enemyCurrentNum = Random.Range(1, 10);
+        enemySelectedCardGameObject.transform.GetChild(0).GetComponent<Text>().text = enemyCurrentNum.ToString();
     }
 }
