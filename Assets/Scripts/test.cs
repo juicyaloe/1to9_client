fc17f1e7dc4970573e9143e295fc778bdc27f294
als8921 <als8921@naver.com>
2022-09-17 01:47:02 +0900

    서버 연결하는 부분 추가

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -1,25 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+
+using WebSocketSharp;
+using Newtonsoft.Json.Linq;
 
 public class test : MonoBehaviour
 {
-    public GameObject myDeck;
-    GameObject[] myCard = new GameObject[9];
+    private WebSocket ws;
+
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 9; i++)
-        {
-            myCard[i] = myDeck.transform.GetChild(i).gameObject;
-            myCard[i].transform.GetChild(0).GetComponent<Text>().text = i.ToString();
-        }
+        ws = new WebSocket("ws://localhost:8000/");
+        ws.Connect();
+
+        ws.OnMessage += (sender, e) => {
+            JObject json = JObject.Parse(e.Data);
+            var a = json.GetValue("data");
+
+            Debug.Log("주소 :  "+((WebSocket)sender).Url+", 데이터 : "+a);
+
+        };
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (ws == null)
+        {
+            Debug.Log("연결 x");
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var json = JObject.FromObject(new {data = "안녕"});
+            var str = json.ToString();
+            ws.Send(str);
+        }
     }
 }
